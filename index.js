e6b3346cc42dd478708b5cd3d377031e809176d2
xanf-code <darshanaswath@gmail.com>
2021-06-29 21:01:12 +0530

    MVP🧨

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
 
-const { MONGODB } = require('./config.js');
+const PORT = process.env.PORT || 5000
+require('dotenv').config();
 const typeDefs = require('./graphql/typeDefs')
 const resolvers = require('./graphql/resolvers');
 
@@ -9,12 +10,14 @@ const server = new ApolloServer({
     typeDefs, resolvers
 });
 
-mongoose.connect(MONGODB, {
+let mongoURL = process.env.MONGODB;
+
+mongoose.connect(mongoURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
     console.log(`MongoDB Connected`)
-    return server.listen({ port: process.env.PORT || 5000 })
+    return server.listen({ port: PORT })
 }).then(
     (res) => console.log(`server running at ${res.url}`)
 ).catch((err) => {
